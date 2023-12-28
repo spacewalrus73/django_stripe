0a39fe049956dc0e1dd82342a5bb6c3db65ede13
Даниил Балабанов <spacewalrus@bk.ru>
2023-12-28 23:10:33 +0300

    add PORT

--- dj_stripe/settings.py
+++ dj_stripe/settings.py
@@ -125,4 +125,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 
-DOMAIN = "http://localhost:8000"
+PORT = '8000' if DEBUG else os.getenv("RENDER_PORT")
+
+DOMAIN = f"http://localhost:{PORT}"
