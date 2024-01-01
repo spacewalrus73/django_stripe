67e24405b77d6e1ed3d6f7c8366e51545f4fdc3a
Даниил Балабанов <spacewalrus@bk.ru>
2024-01-01 22:07:32 +0300

    fix statement for PORT

--- dj_stripe/settings.py
+++ dj_stripe/settings.py
@@ -125,6 +125,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 
-PORT = '8000' if DEBUG else os.getenv("RENDER_PORT")
+PORT = os.getenv("RENDER_PORT") if DEBUG else "8000"
 
 DOMAIN = f"http://localhost:{PORT}"
